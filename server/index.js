d30dcdda6f998efaacec5a120e2918319676241f
Raj yadav <yraj56327@gmail.com>
2026-05-05 01:53:23 +0530

    re

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const app = express();
 const http = require("http")
 const cors = require("cors")
 const { Server } = require("socket.io");
+const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 
@@ -44,7 +45,7 @@ socket.on("disconnect", ()=>{
 
 })
 
-server.listen(3001, ()=>{
+server.listen(PORT  , ()=>{
     console.log("SERVER IS RUNNING ");
 
 })
