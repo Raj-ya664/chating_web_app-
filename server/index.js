b2e39f9214cfcfb35225dc5882caa69f27ddd5e0
Raj yadav <yraj56327@gmail.com>
2026-05-05 01:39:27 +0530

    url changed

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.get('/', (req, res) => {
 const server = http.createServer(app);
 const io = new Server(server, {
     cors :{
-        origin: "http://localhost:3000",
+        origin: "https://chating-web-app-phi.vercel.app/",
         methods : ["GET","POST"],
     },
 
